d5861f62107fcb1bc5cb32f10c46c1036b6858d6
navishapatoliya <navisha@victortyinfotech.com>
2021-07-16 18:00:50 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import RootContainer from './container/RootContainer';
 import Facebook from './container/Facebook';
 import Google from './container/Google';
+// import Instagram from './container/Instagram';
+import GMap from './container/GMap'
 import {
   BrowserRouter as Router,
   Switch,
@@ -22,6 +24,8 @@ function App() {
         
         <h1><Link to="/login-with-google">google</Link></h1>
         <h1><Link to="/login-with-facebook">facebook</Link></h1>
+        {/* <h1><Link to="/login-with-instagram">instagram</Link></h1> */}
+        <h1><Link to="/login-with-Gmap">GMap</Link></h1>
       </div>
         <Switch>
             <Route exact path="/">
@@ -32,7 +36,13 @@ function App() {
             </Route>
               <Route exact path="/login-with-google" >
                 <Google/>
-              </Route>     
+              </Route>  
+              {/* <Route exact path="/login-with-instagram" >
+                <Instagram/>
+              </Route>    */}
+              <Route exact path="/login-with-GMap" >
+                <GMap/>
+              </Route>   
         </Switch>
       </Router>
      </div>
